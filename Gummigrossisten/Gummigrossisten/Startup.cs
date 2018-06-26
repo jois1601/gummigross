2cd6dd2fb2c975a46190f43de9306f8957501356
Joakim Isberg <Joakim Isberg@LAPTOP-LMKU5PFC>
2018-06-26 10:24:33 +0200

    Start

---
+++ Gummigrossisten/Gummigrossisten/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gummigrossisten.Startup))]
+namespace Gummigrossisten
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
