8377b2b477f5bd7cb36985136e2cb65755c44322
marcus2310 <lindberg__94@hotmail.com>
2018-06-26 16:02:16 +0200

    vi har kommit en bit

--- Gummigrossisten/Gummigrossisten/Controllers/HomeController.cs
+++ Gummigrossisten/Gummigrossisten/Controllers/HomeController.cs
@@ -3,23 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using Gummigrossisten.DbOps;
+using Gummigrossisten.Models;
 
 namespace Gummigrossisten.Controllers
 {
     public class HomeController : Controller
     {
+        private DbOp dbc = new DbOp();
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
         {
-            ViewBag.Message = "Your application description page.";
+            TempData["felinlogg"] = "";
+            var username = form["username"];
+            var password = form["password"];
 
+            var user = dbc.LogIn(username, password);
+
+            if(user == null)
+            {
+                TempData["felinlogg"] = "Felaktiga inloggningsuppgifter!";
+                return View();
+            }
+            else
+            {
+                TempData["felinlogg"] = "";
+                return RedirectToAction("Start", "Home", new { id = user.userID});
+            }
+
+        }
+        public ActionResult Login()
+        {
             return View();
         }
+        public ActionResult Start(int id)
+        {
+            user theuser = new user();
+            theuser = dbc.GetUser(id);
+            string search = "";
+            TempData["username"] = theuser.username;
+            TempData["userID"] = theuser.userID;
+            TempData["price"] = theuser.fk_access_id.ToString();
+            List<tire> tirelist = dbc.GetAlltires(search);
+            return View(tirelist);
+        }
+        [HttpPost]
+        public ActionResult Start(FormCollection form)
+        {
+            //Innan vi listar ut däcken måste vi försöka spara vem som är inloggad på ett bra sätt!! Cookie?
+            var search = form["search"];
+           
+            List<tire> tirelist = dbc.GetAlltires(search);
+            return View(tirelist);
+        }
+        public ActionResult Settings(int id)
+        {
+            user user = new user();
+            user = dbc.GetUser(id);
 
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Settings(FormCollection form)
+        {
+            var ID = form["userid"];
+            var userID = ID;
+            user user = new user();
+            user = dbc.GetUser(Convert.ToInt32(userID));
+            
+            var username = form["username"];
+            var currentpass = form["currentpassword"];
+            var newpass = form["newpass"];
+            var newpassagain = form["newpassagain"];
+
+            if(user.password == currentpass && newpass == newpassagain)
+            {
+                user.password = newpass;
+                TempData["correct"] = "Du har nu bytt lösenord!";
+                TempData["felsettings"] = "";
+
+                dbc.UpdateUser(user);
+                return View(user);
+            }
+            else
+            {
+                TempData["correct"] = "";
+                TempData["felsettings"] = "Du har skrivit in fel uppgifter!";
+                return View(user);
+
+            }
+
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ Gummigrossisten/Gummigrossisten/DbOps/DbOp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gummigrossisten.Models;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+
+
+namespace Gummigrossisten.DbOps
+{
+    public class DbOp
+    {
+        private gummi_grossisten_se_dbEntities db = new gummi_grossisten_se_dbEntities();
+
+
+
+        public user LogIn(string username, string password)
+        {
+            user user = new user();
+            var username1 = username.ToLower();
+            var users = db.user.Where(x => x.username == username1 && x.password == password).ToList();
+            if (users.Count == 1)
+            {
+                foreach (var item in users)
+                {
+                    user = item;
+                }
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public user GetUser(int id)
+        {
+            user user = new user();
+            try
+            {
+                user = db.user.Find(id);
+                return user;
+
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+        public List<tire> GetAlltires(string search)
+        {
+            if(search != "")
+            {
+                var tireslist = db.tire.Where(x => x.dimension.Contains(search)).ToList();
+                List<tire> SortedList = tireslist.OrderByDescending(x => x.balance).ToList();
+                return SortedList;
+            }
+            else
+            {
+                var tires = db.tire.ToList();
+                List<tire> SortedList = tires.OrderByDescending(x => x.balance).ToList();
+
+                return SortedList;
+
+            }
+            
+
+        }
+        public void UpdateUser(user user)
+        {
+
+            try
+            {
+                user theuser = db.user.Single(x => x.userID == user.userID);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+        }
+    }
+}
